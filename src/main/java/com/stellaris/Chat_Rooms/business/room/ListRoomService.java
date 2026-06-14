52edf5cab1520e1261b186eea31b9fbc2f8b9b0e
Thauã Gabriel <tagashi.dev@gmail.com>
2026-06-14 01:44:58 -0300

    feat: list rooms

---
+++ src/main/java/com/stellaris/Chat_Rooms/business/room/ListRoomService.java
@@ -0,0 +1,23 @@
+package com.stellaris.Chat_Rooms.business.room;
+
+import com.stellaris.Chat_Rooms.http.dto.response.room.RoomResponseDTO;
+import com.stellaris.Chat_Rooms.persistence.mappers.RoomMapper;
+import com.stellaris.Chat_Rooms.persistence.repositories.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ListRoomService {
+    private final RoomRepository roomRepository;
+    private final RoomMapper roomMapper;
+
+    public List<RoomResponseDTO> list() {
+        return roomRepository.findAll()
+                .stream()
+                .map(roomMapper::map)
+                .toList();
+    }
+}
--- src/main/java/com/stellaris/Chat_Rooms/http/controllers/RoomController.java
+++ src/main/java/com/stellaris/Chat_Rooms/http/controllers/RoomController.java
@@ -1,6 +1,7 @@
 package com.stellaris.Chat_Rooms.http.controllers;
 
 import com.stellaris.Chat_Rooms.business.room.CreateRoomService;
+import com.stellaris.Chat_Rooms.business.room.ListRoomService;
 import com.stellaris.Chat_Rooms.http.dto.response.room.CreateRoomRequestDTO;
 import com.stellaris.Chat_Rooms.http.dto.response.room.RoomResponseDTO;
 import com.stellaris.Chat_Rooms.persistence.entities.UserEntity;
@@ -8,16 +9,16 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
 @RequiredArgsConstructor
 public class RoomController {
     private final CreateRoomService createRoomService;
+    private final ListRoomService listRoomService;
 
     @PostMapping
     public ResponseEntity<RoomResponseDTO> createRoom(
@@ -25,5 +26,10 @@ public class RoomController {
             @RequestBody @Valid CreateRoomRequestDTO request
     ) {
         return ResponseEntity.ok(createRoomService.create(currentUser, request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoomResponseDTO>> listRooms() {
+        return ResponseEntity.ok(listRoomService.list());
     }
 }
